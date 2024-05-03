8148e8e695c5552f8ad3449ed302c5aea09c5760
velpcode <velpmail@gmail.com>
2024-05-03 18:21:17 -0400

    Reactdash

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
 
-      <h1>
-        hi
-      </h1>
+      <Button>Click me</Button>
 
     </div>
   );
---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Sidebar() {
+
+    return (
+        <div className="w-[300px] border-r h-screen p-5">
+            Sidebar
+        </div>
+    )
+
+}
