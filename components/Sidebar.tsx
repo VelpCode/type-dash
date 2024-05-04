e7990ae4ed943aab2cd79c8e8439c11d90fb96a6
velpcode <velpmail@gmail.com>
2024-05-03 22:59:04 -0400

    sourcecode

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import UserItem from "./UserItem";
+
 export default function Sidebar() {
 
     return (
-        <div className="w-[300px] border-r h-screen p-5">
-            Sidebar
+        <div className="w-[300px] border-r h-screen flex flex-col min-h-screen p-4 ">
+            <UserItem />
+            <div>User Part</div>
+            <div className = "grow">Menu</div>
         </div>
     )
 
---
+++ components/UserItem.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function UserItem() {
+    return <div>
+        <p>Velp</p>
+        <p>velpmail@gmail.com</p>
+    </div>
+}
+
+
